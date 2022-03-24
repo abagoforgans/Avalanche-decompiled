e44ae4cfd4ea8a748cf75a7033faec1978b61028
bot <abagoforgans@users.noreply.github.com>
2022-03-24 14:45:22 +0000

    autocommit

---
+++ contracts/0x0f4A657FBF7Ee3c8852Ab426d3A2145938B1b983.sol
@@ -0,0 +1,55 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+array of struct stor0;
+
+function _fallback() payable {
+    revert
+}
+
+function sub_c9843d3a(?) payable {
+    require calldata.size - 4 >= 64
+    create contract with 0 wei
+                    code: code.data[686 len 5608], msg.sender, address(arg1), arg2
+    if not create.new_address:
+        revert with ext_call.return_data[0 len return_data.size]
+    stor0[msg.sender].field_0++
+    stor0[msg.sender][stor0[msg.sender].field_0].field_0 = address(create.new_address)
+    call address(create.new_address) with:
+       value msg.value wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    emit Created(address(create.new_address), msg.sender, msg.value);
+    return address(create.new_address)
+}
+
+function getWallets(address arg1) {
+    require calldata.size - 4 >= 32
+    if not stor0[address(arg1)].field_0:
+        mem[(32 * stor0[address(arg1)].field_0) + 128] = 32
+        mem[(32 * stor0[address(arg1)].field_0) + 160] = stor0[address(arg1)].field_0
+        mem[(32 * stor0[address(arg1)].field_0) + 192 len floor32(stor0[address(arg1)].field_0)] = mem[128 len floor32(stor0[address(arg1)].field_0)]
+        return memory
+          from (32 * stor0[address(arg1)].field_0) + 128
+           len (96 * stor0[address(arg1)].field_0) + 64
+    mem[128] = stor0[address(arg1)].field_0
+    idx = 128
+    s = 0
+    while (32 * stor0[address(arg1)].field_0) + 96 > idx:
+        mem[idx + 32] = stor0[address(arg1)][s].field_256
+        idx = idx + 32
+        s = s + 1
+        continue 
+    mem[(32 * stor0[address(arg1)].field_0) + 192 len floor32(stor0[address(arg1)].field_0)] = mem[128 len floor32(stor0[address(arg1)].field_0)]
+    return Array(len=stor0[address(arg1)].field_0, data=mem[128 len floor32(stor0[address(arg1)].field_0)], mem[(32 * stor0[address(arg1)].field_0) + floor32(stor0[address(arg1)].field_0) + 192 len (32 * stor0[address(arg1)].field_0) - floor32(stor0[address(arg1)].field_0)]), 
+}
+
+
+
+}
---
+++ contracts/0x67a57535b11445506a9e340662CD0c9755E5b1b4.sol
@@ -0,0 +1,44 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address stor0;
+
+function _fallback() payable {
+    revert
+}
+
+function sub_77d2713f(?) {
+    require calldata.size - 4 >= 64
+    require arg1 == address(arg1)
+    require arg2 <= test266151307()
+    require arg2 + 35 < calldata.size
+    require arg2.length <= test266151307()
+    require arg2 + arg2.length + 36 <= calldata.size
+    require msg.sender == stor0
+    call address(arg1) with:
+         gas gas_remaining wei
+        args arg2[all]
+}
+
+function sub_49476389(?) {
+    require calldata.size - 4 >= 96
+    require arg1 == address(arg1)
+    require arg2 <= test266151307()
+    require arg2 + 35 < calldata.size
+    require arg2.length <= test266151307()
+    require arg2 + arg2.length + 36 <= calldata.size
+    require msg.sender == stor0
+    call address(arg1) with:
+       value arg3 wei
+         gas gas_remaining wei
+        args arg2[all]
+}
+
+
+
+}
