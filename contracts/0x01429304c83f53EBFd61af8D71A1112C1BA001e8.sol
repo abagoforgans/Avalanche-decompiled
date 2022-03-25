4289506cff24b1ca7fc16c7c0aa4c7f0f6dcd815
bot <abagoforgans@users.noreply.github.com>
2022-03-25 06:51:59 +0000

    autocommit

---
+++ contracts/0x01429304c83f53EBFd61af8D71A1112C1BA001e8.sol
@@ -0,0 +1,172 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+function _fallback() payable {
+    revert
+}
+
+function sub_3a204709(?) payable {
+    require calldata.size - 4 >= 64
+    require cd[4] == address(cd[4])
+    require cd[36] <= test266151307()
+    require cd[36] + 35 < calldata.size
+    require ('cd', 36).length <= test266151307()
+    require (32 * ('cd', 36).length) + 128 <= test266151307() and (32 * ('cd', 36).length) + 128 >= 96
+    mem[64] = (32 * ('cd', 36).length) + 128
+    mem[96] = ('cd', 36).length
+    require cd[36] + (32 * ('cd', 36).length) + 36 <= calldata.size
+    idx = 0
+    s = cd[36] + 36
+    t = 128
+    while idx < ('cd', 36).length:
+        require cd[s] == address(cd[s])
+        mem[t] = cd[s]
+        idx = idx + 1
+        s = s + 32
+        t = t + 32
+        continue 
+    idx = 0
+    while idx < ('cd', 36).length:
+        require idx < mem[96]
+        _29 = mem[(32 * idx) + 128]
+        mem[mem[64] + 4] = address(cd[4])
+        require ext_code.size(address(_29))
+        staticcall address(_29).0x70a08231 with:
+                gas gas_remaining wei
+               args address(cd[4])
+        mem[mem[64]] = ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        _34 = mem[64]
+        mem[64] = mem[64] + ceil32(return_data.size)
+        require return_data.size >= 32
+        require mem[_34] == mem[_34]
+        require idx < mem[96]
+        mem[(32 * idx) + 128] = mem[_34]
+        idx = idx + 1
+        continue 
+    mem[mem[64]] = 32
+    mem[mem[64] + 32] = mem[96]
+    mem[mem[64] + 64 len 32 * mem[96]] = mem[128 len 32 * mem[96]]
+    return 32, mem[mem[64] + 32 len (32 * mem[96]) + 32]
+}
+
+function sub_0b5cb275(?) payable {
+    require calldata.size - 4 >= 64
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require (32 * ('cd', 4).length) + 128 <= test266151307() and (32 * ('cd', 4).length) + 128 >= 96
+    mem[64] = (32 * ('cd', 4).length) + 128
+    mem[96] = ('cd', 4).length
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    idx = 0
+    s = cd[4] + 36
+    t = 128
+    while idx < ('cd', 4).length:
+        require cd[s] == address(cd[s])
+        mem[t] = cd[s]
+        idx = idx + 1
+        s = s + 32
+        t = t + 32
+        continue 
+    require cd[36] == address(cd[36])
+    idx = 0
+    while idx < ('cd', 4).length:
+        require idx < mem[96]
+        _29 = mem[(32 * idx) + 128]
+        mem[mem[64] + 4] = mem[(32 * idx) + 140 len 20]
+        require ext_code.size(address(cd[36]))
+        staticcall address(cd[36]).0x70a08231 with:
+                gas gas_remaining wei
+               args address(_29)
+        mem[mem[64]] = ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        _34 = mem[64]
+        mem[64] = mem[64] + ceil32(return_data.size)
+        require return_data.size >= 32
+        require mem[_34] == mem[_34]
+        require idx < mem[96]
+        mem[(32 * idx) + 128] = mem[_34]
+        idx = idx + 1
+        continue 
+    mem[mem[64]] = 32
+    mem[mem[64] + 32] = mem[96]
+    mem[mem[64] + 64 len 32 * mem[96]] = mem[128 len 32 * mem[96]]
+    return 32, mem[mem[64] + 32 len (32 * mem[96]) + 32]
+}
+
+function sub_7dbb6f26(?) payable {
+    require calldata.size - 4 >= 64
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require (32 * ('cd', 4).length) + 128 <= test266151307() and (32 * ('cd', 4).length) + 128 >= 96
+    mem[96] = ('cd', 4).length
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    idx = 0
+    s = cd[4] + 36
+    t = 128
+    while idx < ('cd', 4).length:
+        require cd[s] == address(cd[s])
+        mem[t] = cd[s]
+        idx = idx + 1
+        s = s + 32
+        t = t + 32
+        continue 
+    require cd[36] <= test266151307()
+    require cd[36] + 35 < calldata.size
+    require ('cd', 36).length <= test266151307()
+    require (32 * ('cd', 4).length) + (32 * ('cd', 36).length) + 160 <= test266151307() and (32 * ('cd', 36).length) + 160 >= 128
+    mem[64] = (32 * ('cd', 4).length) + (32 * ('cd', 36).length) + 160
+    mem[(32 * ('cd', 4).length) + 128] = ('cd', 36).length
+    require cd[36] + (32 * ('cd', 36).length) + 36 <= calldata.size
+    idx = 0
+    s = cd[36] + 36
+    t = (32 * ('cd', 4).length) + 160
+    while idx < ('cd', 36).length:
+        require cd[s] == address(cd[s])
+        mem[t] = cd[s]
+        idx = idx + 1
+        s = s + 32
+        t = t + 32
+        continue 
+    if ('cd', 4).length != ('cd', 36).length:
+        revert with 0, 'not same length'
+    idx = 0
+    while idx < ('cd', 36).length:
+        require idx < mem[(32 * ('cd', 4).length) + 128]
+        _61 = mem[(32 * idx) + (32 * ('cd', 4).length) + 160]
+        require idx < mem[96]
+        _63 = mem[(32 * idx) + 128]
+        mem[mem[64] + 4] = mem[(32 * idx) + 140 len 20]
+        require ext_code.size(address(_61))
+        staticcall address(_61).0x70a08231 with:
+                gas gas_remaining wei
+               args address(_63)
+        mem[mem[64]] = ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        _68 = mem[64]
+        mem[64] = mem[64] + ceil32(return_data.size)
+        require return_data.size >= 32
+        require mem[_68] == mem[_68]
+        require idx < mem[96]
+        mem[(32 * idx) + 128] = mem[_68]
+        idx = idx + 1
+        continue 
+    mem[mem[64]] = 32
+    mem[mem[64] + 32] = mem[96]
+    mem[mem[64] + 64 len 32 * mem[96]] = mem[128 len 32 * mem[96]]
+    return 32, mem[mem[64] + 32 len (32 * mem[96]) + 32]
+}
+
+
+
+}
