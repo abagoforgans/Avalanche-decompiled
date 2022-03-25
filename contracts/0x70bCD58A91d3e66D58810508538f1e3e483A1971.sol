d4d1c17399ff7a58f8b68791aff5e8449484560d
bot <abagoforgans@users.noreply.github.com>
2022-03-25 22:11:34 +0000

    autocommit

---
+++ contracts/0x70bCD58A91d3e66D58810508538f1e3e483A1971.sol
@@ -0,0 +1,83 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - requestPrice(address arg1, string arg2)
+#
+address stor2;
+mapping of address stor5;
+address owner;
+uint256 price;
+
+function owner() payable {
+    return owner
+}
+
+function price() payable {
+    return price
+}
+
+function _fallback() payable {
+    revert
+}
+
+function isOwner() payable {
+    return (msg.sender == owner)
+}
+
+function fulfill(bytes32 arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if stor5[arg1] != msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    40,
+                    0x44536f75726365206d75737420626520746865206f7261636c65206f662074686520726571756573,
+                    mem[204 len 24]
+    stor5[arg1] = 0
+    emit ChainlinkFulfilled(arg1);
+    price = arg2
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0xfe4f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function withdrawLink() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    require ext_code.size(stor2)
+    staticcall stor2.0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(stor2)
+    call stor2.transfer(address rg1, uint256 rg2) with:
+         gas gas_remaining wei
+        args msg.sender, ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if not ext_call.return_data[0]:
+        revert with 0, 'Unable to transfer'
+}
+
+
+
+}
