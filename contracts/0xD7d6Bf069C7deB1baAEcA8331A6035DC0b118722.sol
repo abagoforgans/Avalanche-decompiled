830b6c2945a7e77a587ae883db90bee86e50a044
bot <abagoforgans@users.noreply.github.com>
2022-03-25 20:02:24 +0000

    autocommit

---
+++ contracts/0xD7d6Bf069C7deB1baAEcA8331A6035DC0b118722.sol
@@ -0,0 +1,37 @@
+contract main {
+
+
+// =======================  Init code  ======================
+
+
+uint256 stor0;
+
+function _fallback() payable {
+    stor0 = 0
+    require not msg.value
+    return code.data[35 len 317]
+}
+
+
+
+// =====================  Runtime code  =====================
+
+
+uint256 myValue;
+
+function myValue() {
+    return myValue
+}
+
+function _fallback() payable {
+    revert
+}
+
+function Execute(uint256 arg1) {
+    myValue = arg1
+    emit 0x8c03554b: msg.sender, arg1
+}
+
+
+
+}
