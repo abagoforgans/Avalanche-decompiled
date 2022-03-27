52a19407efdddf2049e63a42203d3bbfa90e1054
bot <abagoforgans@users.noreply.github.com>
2022-03-27 19:44:14 +0000

    autocommit

---
+++ contracts/0x10B34Bd0d3b4532BE749b39Aae4B01d229e538E3.sol
@@ -0,0 +1,68 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address sub_a012e804Address;
+uint256 sub_e7f05644;
+
+function sub_a012e804(?) payable {
+    return sub_a012e804Address
+}
+
+function sub_e7f05644(?) payable {
+    return sub_e7f05644
+}
+
+function _fallback() payable {
+    revert
+}
+
+function balanceOf(address arg1) payable {
+    require calldata.size - 4 >= 32
+    require ext_code.size(sub_a012e804Address)
+    staticcall sub_a012e804Address.userInfo(uint256 rg1, address rg2) with:
+            gas gas_remaining wei
+           args sub_e7f05644, arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 64
+    return ext_call.return_data[0]
+}
+
+function earned(address arg1) payable {
+    require calldata.size - 4 >= 32
+    require ext_code.size(sub_a012e804Address)
+    staticcall sub_a012e804Address.pendingGondola(uint256 rg1, address rg2) with:
+            gas gas_remaining wei
+           args sub_e7f05644, arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[0]
+}
+
+function totalSupply() payable {
+    require ext_code.size(sub_a012e804Address)
+    staticcall sub_a012e804Address.0x1526fe27 with:
+            gas gas_remaining wei
+           args sub_e7f05644
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 128
+    require ext_code.size(address(ext_call.return_data[0]))
+    staticcall address(ext_call.return_data[0]).0x70a08231 with:
+            gas gas_remaining wei
+           args sub_a012e804Address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[0]
+}
+
+
+
+}
