353f06bb902a8ea035809307a78dacb7af4e6e2c
bot <abagoforgans@users.noreply.github.com>
2022-03-28 06:33:35 +0000

    autocommit

---
+++ contracts/0xaAD41CCf76c035DAF80d662584823215f2e0487D.sol
@@ -0,0 +1,176 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+array of struct sub_9c90ba49;
+mapping of uint8 stor1;
+mapping of uint8 stor99;
+
+function sub_9c90ba49(?) payable {
+    require calldata.size - 4 >= 32
+    return uint256(sub_9c90ba49[address(arg1)].field_768)
+}
+
+function isRegistered(address arg1) payable {
+    require calldata.size - 4 >= 32
+    return bool(sub_9c90ba49[address(arg1)].field_512)
+}
+
+function sub_e530dd03(?) payable {
+    return uint256(sub_9c90ba49[address(arg1)][1][0 len sub_9c90ba49[address(arg1)][1].length].field_0)
+}
+
+function sub_ecd01595(?) payable {
+    return uint256(sub_9c90ba49[address(arg1)][0 len sub_9c90ba49[address(arg1)].length].field_0)
+}
+
+function _fallback() payable {
+    revert
+}
+
+function sub_fec2f8a7(?) payable {
+    require calldata.size - 4 >= 32
+    uint256(sub_9c90ba49[address(msg.sender)].field_768) = arg1
+}
+
+function sub_6aa2afd5(?) payable {
+    require calldata.size - 4 >= 32
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + arg1.length + 36 <= calldata.size
+    uint256(sub_9c90ba49[address(msg.sender)][1][].field_0) = Array(len=arg1.length, data=arg1[all])
+}
+
+function sub_64ac8dd1(?) payable {
+    require calldata.size - 4 >= 32
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + arg1.length + 36 <= calldata.size
+    mem[ceil32(arg1.length) + 128] = bool(stor1[arg1[all]])
+    return Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]
+}
+
+function sub_4a3c83cf(?) payable {
+    require calldata.size - 4 >= 96
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + arg1.length + 36 <= calldata.size
+    mem[128 len arg1.length] = arg1[all]
+    mem[arg1.length + 128] = 0
+    require arg2 <= 4294967296
+    require arg2 + 36 <= calldata.size
+    require arg2.length <= 4294967296 and arg2 + arg2.length + 36 <= calldata.size
+    mem[ceil32(arg1.length) + 128] = arg2.length
+    mem[ceil32(arg1.length) + 160 len arg2.length] = arg2[all]
+    mem[ceil32(arg1.length) + arg2.length + 160] = 0
+    require bool(sub_9c90ba49[address(msg.sender)].field_512) != 1
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + 160 len floor32(arg1.length)] = call.data[arg1 + 36 len floor32(arg1.length)]
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + floor32(arg1.length) + -(arg1.length % 32) + 192 len arg1.length % 32] = mem[-(arg1.length % 32) + floor32(arg1.length) + 160 len arg1.length % 32]
+    mem[arg1.length + ceil32(arg1.length) + ceil32(arg2.length) + 160] = 1
+    if bool(stor[mem[ceil32(arg1.length) + arg2.length + 192 len arg1.length - arg2.length + ceil32(arg2.length)]][Mask(8 * -ceil32(arg2.length) + arg2.length + 32, 0, 0)]) == 1:
+        revert with 0, 
+                    32,
+                    55,
+                    0xfe43686f6f7365206120646966666572656e74206e69636b6e616d652c2074686174206f6e6520697320616c72656164792074616b656e,
+                    mem[ceil32(arg1.length) + ceil32(arg2.length) + 283 len 9]
+    uint256(sub_9c90ba49[address(msg.sender)][].field_0) = Array(len=arg1.length, data=arg1[all])
+    uint256(sub_9c90ba49[address(msg.sender)][1][].field_0) = Array(len=Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)], data=mem[ceil32(arg1.length) + 160 len Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]])
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + 160 len floor32(arg1.length)] = call.data[arg1 + 36 len floor32(arg1.length)]
+    mem[ceil32(arg1.length) + ceil32(arg2.length) + floor32(arg1.length) + -(arg1.length % 32) + 192 len arg1.length % 32] = mem[-(arg1.length % 32) + floor32(arg1.length) + 160 len arg1.length % 32]
+    mem[arg1.length + ceil32(arg1.length) + ceil32(arg2.length) + 160] = 1
+    stor[mem[ceil32(arg1.length) + arg2.length + 192 len arg1.length - arg2.length + ceil32(arg2.length)]][Mask(8 * -ceil32(arg2.length) + arg2.length + 32, 0, 0)] = 1
+    uint256(sub_9c90ba49[address(msg.sender)].field_768) = arg3
+    sub_9c90ba49[address(msg.sender)].field_512 = 1
+}
+
+function sub_cb1d2b51(?) payable {
+    require calldata.size - 4 >= 32
+    mem[32] = 0
+    mem[96] = sub_9c90ba49[arg1].length
+    mem[0] = sha3(arg1, 0)
+    mem[128] = uint256(sub_9c90ba49[arg1].field_0)
+    idx = 128
+    s = 0
+    while sub_9c90ba49[arg1].length + 96 > idx:
+        mem[idx + 32] = uint256(sub_9c90ba49[arg1][s].field_256)
+        idx = idx + 32
+        s = s + 1
+        continue 
+    mem[64] = ceil32(sub_9c90ba49[arg1].length) + ceil32(sub_9c90ba49[arg1][1].length) + 160
+    mem[ceil32(sub_9c90ba49[arg1].length) + 128] = sub_9c90ba49[arg1][1].length
+    mem[ceil32(sub_9c90ba49[arg1].length) + 160] = uint256(sub_9c90ba49[arg1][1].field_0)
+    idx = ceil32(sub_9c90ba49[arg1].length) + 160
+    s = 0
+    while ceil32(sub_9c90ba49[arg1].length) + sub_9c90ba49[arg1][1].length + 128 > idx:
+        mem[idx + 32] = uint256(sub_9c90ba49[arg1][s + 1].field_256)
+        idx = idx + 32
+        s = s + 1
+        continue 
+    mem[ceil32(sub_9c90ba49[arg1].length) + ceil32(sub_9c90ba49[arg1][1].length) + 224] = bool(sub_9c90ba49[arg1].field_512)
+    mem[ceil32(sub_9c90ba49[arg1].length) + ceil32(sub_9c90ba49[arg1][1].length) + 256] = uint256(sub_9c90ba49[arg1].field_768)
+    mem[ceil32(sub_9c90ba49[arg1].length) + ceil32(sub_9c90ba49[arg1][1].length) + 160] = 128
+    mem[ceil32(sub_9c90ba49[arg1].length) + ceil32(sub_9c90ba49[arg1][1].length) + 288] = sub_9c90ba49[arg1].length
+    mem[ceil32(sub_9c90ba49[arg1].length) + ceil32(sub_9c90ba49[arg1][1].length) + 320 len ceil32(sub_9c90ba49[arg1].length)] = mem[128 len ceil32(sub_9c90ba49[arg1].length)]
+    mem[ceil32(sub_9c90ba49[arg1].length) + ceil32(sub_9c90ba49[arg1][1].length) + 192] = sub_9c90ba49[arg1].length + 160
+    mem[sub_9c90ba49[arg1].length + ceil32(sub_9c90ba49[arg1].length) + ceil32(sub_9c90ba49[arg1][1].length) + 320] = sub_9c90ba49[arg1][1].length
+    mem[sub_9c90ba49[arg1].length + ceil32(sub_9c90ba49[arg1].length) + ceil32(sub_9c90ba49[arg1][1].length) + 352 len ceil32(sub_9c90ba49[arg1][1].length)] = mem[ceil32(sub_9c90ba49[arg1].length) + 160 len ceil32(sub_9c90ba49[arg1][1].length)]
+    if not sub_9c90ba49[arg1][1].length % 32:
+        return Array(len=sub_9c90ba49[arg1].length, data=mem[128 len ceil32(sub_9c90ba49[arg1].length)], mem[(2 * ceil32(sub_9c90ba49[arg1].length)) + ceil32(sub_9c90ba49[arg1][1].length) + 320 len sub_9c90ba49[arg1][1].length + sub_9c90ba49[arg1].length + -ceil32(sub_9c90ba49[arg1].length) + 32]), 
+               sub_9c90ba49[arg1].length + 160,
+               bool(sub_9c90ba49[arg1].field_512),
+               uint256(sub_9c90ba49[arg1].field_768)
+    mem[floor32(sub_9c90ba49[arg1][1].length) + sub_9c90ba49[arg1].length + ceil32(sub_9c90ba49[arg1].length) + ceil32(sub_9c90ba49[arg1][1].length) + 352] = mem[floor32(sub_9c90ba49[arg1][1].length) + sub_9c90ba49[arg1].length + ceil32(sub_9c90ba49[arg1].length) + ceil32(sub_9c90ba49[arg1][1].length) + -sub_9c90ba49[arg1][1].length % 32 + 384 len sub_9c90ba49[arg1][1].length % 32]
+    return Array(len=sub_9c90ba49[arg1].length, data=mem[128 len ceil32(sub_9c90ba49[arg1].length)], mem[(2 * ceil32(sub_9c90ba49[arg1].length)) + ceil32(sub_9c90ba49[arg1][1].length) + 320 len floor32(sub_9c90ba49[arg1][1].length) + sub_9c90ba49[arg1].length + -ceil32(sub_9c90ba49[arg1].length) + 64]), 
+           sub_9c90ba49[arg1].length + 160,
+           bool(sub_9c90ba49[arg1].field_512),
+           uint256(sub_9c90ba49[arg1].field_768)
+}
+
+function sub_a1157cde(?) payable {
+    require calldata.size - 4 >= 32
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + arg1.length + 36 <= calldata.size
+    mem[96] = arg1.length
+    mem[128 len arg1.length] = arg1[all]
+    mem[arg1.length + 128] = 0
+    mem[ceil32(arg1.length) + 128 len floor32(arg1.length)] = call.data[arg1 + 36 len floor32(arg1.length)]
+    mem[ceil32(arg1.length) + floor32(arg1.length) + -(arg1.length % 32) + 160 len arg1.length % 32] = mem[-(arg1.length % 32) + floor32(arg1.length) + 160 len arg1.length % 32]
+    mem[arg1.length + ceil32(arg1.length) + 128] = 1
+    if bool(stor[mem[arg1.length + 160 len ceil32(arg1.length)]][Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0)]) == 1:
+        revert with 0, 
+                    32,
+                    55,
+                    0xfe43686f6f7365206120646966666572656e74206e69636b6e616d652c2074686174206f6e6520697320616c72656164792074616b656e,
+                    mem[ceil32(arg1.length) + 251 len 9]
+    mem[32] = 0
+    mem[64] = ceil32(arg1.length) + ceil32(sub_9c90ba49[address(msg.sender)].length) + 160
+    mem[ceil32(arg1.length) + 128] = sub_9c90ba49[address(msg.sender)].length
+    mem[0] = sha3(address(msg.sender), 0)
+    mem[ceil32(arg1.length) + 160] = uint256(sub_9c90ba49[address(msg.sender)].field_0)
+    idx = ceil32(arg1.length) + 160
+    s = 0
+    while ceil32(arg1.length) + sub_9c90ba49[address(msg.sender)].length + 128 > idx:
+        mem[idx + 32] = uint256(sub_9c90ba49[address(msg.sender)][s].field_256)
+        idx = idx + 32
+        s = s + 1
+        continue 
+    _153 = Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]
+    mem[ceil32(arg1.length) + ceil32(sub_9c90ba49[address(msg.sender)].length) + 160 len floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)])] = mem[ceil32(arg1.length) + 160 len floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)])]
+    mem[ceil32(arg1.length) + ceil32(sub_9c90ba49[address(msg.sender)].length) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 160] = mem[ceil32(arg1.length) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 192 len Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32] or Mask(8 * -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 32, -(8 * -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 32) + 256, mem[ceil32(arg1.length) + ceil32(sub_9c90ba49[address(msg.sender)].length) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 160])
+    mem[_153 + ceil32(arg1.length) + ceil32(sub_9c90ba49[address(msg.sender)].length) + 160] = 1
+    stor[sha3(mem[ceil32(arg1.length) + ceil32(stor0[address(msg.sender)].length) + 160 len _153 + 32])] = 0
+    mem[ceil32(arg1.length) + ceil32(sub_9c90ba49[address(msg.sender)].length) + 160 len floor32(arg1.length)] = call.data[arg1 + 36 len floor32(arg1.length)]
+    mem[ceil32(arg1.length) + ceil32(sub_9c90ba49[address(msg.sender)].length) + floor32(arg1.length) + -(arg1.length % 32) + 192 len arg1.length % 32] = mem[floor32(arg1.length) + -(arg1.length % 32) + 160 len arg1.length % 32]
+    mem[arg1.length + ceil32(arg1.length) + ceil32(sub_9c90ba49[address(msg.sender)].length) + 160] = 1
+    stor[mem[ceil32(arg1.length) + ceil32(stor0[address(msg.sender)].length) + floor32(arg1.length) + 160 len (arg1.length % 32) + 32]][call.data[arg1 + 36 len floor32(arg1.length)]] = 1
+    uint256(sub_9c90ba49[address(msg.sender)][].field_0) = Array(len=arg1.length, data=arg1[all])
+}
+
+
+
+}
