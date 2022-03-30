fa389499364082da365fbfe3938ba2f88e4a130d
bot <abagoforgans@users.noreply.github.com>
2022-03-30 11:00:40 +0000

    autocommit

---
+++ contracts/0xfd9FF5c9F1deB4272ca3feA481923d6b5B06e7Ce.sol
@@ -0,0 +1,292 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+function _fallback() payable {
+    revert
+}
+
+function sub_ec82c42d(?) payable {
+    require calldata.size - 4 >= 96
+    mem[64] = 160
+    if arg1 + 10^18 < arg1:
+        revert with 0, 'SafeMath: addition overflow'
+    s = 10^18
+    idx = 1
+    t = arg1 + 10^18
+    while idx:
+        if not bool(idx):
+            if not t:
+                _22 = mem[64]
+                mem[64] = mem[64] + 64
+                mem[_22] = 26
+                mem[_22 + 32] = 'SafeMath: division by zero'
+                s = s
+                idx = idx / 2
+                t = 0
+                continue 
+            if t * t / t != t:
+                revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+            _29 = mem[64]
+            mem[64] = mem[64] + 64
+            mem[_29] = 26
+            mem[_29 + 32] = 'SafeMath: division by zero'
+            s = s
+            idx = idx / 2
+            t = t * t / 10^18
+            continue 
+        if not s:
+            _23 = mem[64]
+            mem[64] = mem[64] + 64
+            mem[_23] = 26
+            mem[_23 + 32] = 'SafeMath: division by zero'
+            if not t:
+                _42 = mem[64]
+                mem[64] = mem[64] + 64
+                mem[_42] = 26
+                mem[_42 + 32] = 'SafeMath: division by zero'
+                s = 0
+                idx = idx / 2
+                t = 0
+                continue 
+            if t * t / t != t:
+                revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+            _48 = mem[64]
+            mem[64] = mem[64] + 64
+            mem[_48] = 26
+            mem[_48 + 32] = 'SafeMath: division by zero'
+            s = 0
+            idx = idx / 2
+            t = t * t / 10^18
+            continue 
+        if t * s / s != t:
+            revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+        _30 = mem[64]
+        mem[64] = mem[64] + 64
+        mem[_30] = 26
+        mem[_30 + 32] = 'SafeMath: division by zero'
+        if not t:
+            _47 = mem[64]
+            mem[64] = mem[64] + 64
+            mem[_47] = 26
+            mem[_47 + 32] = 'SafeMath: division by zero'
+            s = t * s / 10^18
+            idx = idx / 2
+            t = 0
+            continue 
+        if t * t / t != t:
+            revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+        _49 = mem[64]
+        mem[64] = mem[64] + 64
+        mem[_49] = 26
+        mem[_49 + 32] = 'SafeMath: division by zero'
+        s = t * s / 10^18
+        idx = idx / 2
+        t = t * t / 10^18
+        continue 
+    _11 = mem[64]
+    mem[64] = mem[64] + 64
+    mem[_11] = 30
+    mem[_11 + 32] = 'SafeMath: subtraction overflow'
+    if 10^18 > s:
+        revert with 0, 'SafeMath: subtraction overflow'
+    _26 = mem[64]
+    mem[64] = mem[64] + 64
+    mem[_26] = 26
+    mem[_26 + 32] = 'SafeMath: division by zero'
+    if not arg3:
+        revert with 0, 'SafeMath: division by zero'
+    _39 = mem[64]
+    mem[64] = mem[64] + 64
+    mem[_39] = 26
+    mem[_39 + 32] = 'SafeMath: division by zero'
+    if arg2 + 10^18 < arg2:
+        revert with 0, 'SafeMath: addition overflow'
+    t = 10^18
+    idx = 1
+    u = arg2 + 10^18
+    while idx:
+        if not bool(idx):
+            if not u:
+                _76 = mem[64]
+                mem[64] = mem[64] + 64
+                mem[_76] = 26
+                mem[_76 + 32] = 'SafeMath: division by zero'
+                t = t
+                idx = idx / 2
+                u = 0
+                continue 
+            if u * u / u != u:
+                revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+            _83 = mem[64]
+            mem[64] = mem[64] + 64
+            mem[_83] = 26
+            mem[_83 + 32] = 'SafeMath: division by zero'
+            t = t
+            idx = idx / 2
+            u = u * u / 10^18
+            continue 
+        if not t:
+            _77 = mem[64]
+            mem[64] = mem[64] + 64
+            mem[_77] = 26
+            mem[_77 + 32] = 'SafeMath: division by zero'
+            if not u:
+                _96 = mem[64]
+                mem[64] = mem[64] + 64
+                mem[_96] = 26
+                mem[_96 + 32] = 'SafeMath: division by zero'
+                t = 0
+                idx = idx / 2
+                u = 0
+                continue 
+            if u * u / u != u:
+                revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+            _102 = mem[64]
+            mem[64] = mem[64] + 64
+            mem[_102] = 26
+            mem[_102 + 32] = 'SafeMath: division by zero'
+            t = 0
+            idx = idx / 2
+            u = u * u / 10^18
+            continue 
+        if u * t / t != u:
+            revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+        _84 = mem[64]
+        mem[64] = mem[64] + 64
+        mem[_84] = 26
+        mem[_84 + 32] = 'SafeMath: division by zero'
+        if not u:
+            _101 = mem[64]
+            mem[64] = mem[64] + 64
+            mem[_101] = 26
+            mem[_101 + 32] = 'SafeMath: division by zero'
+            t = u * t / 10^18
+            idx = idx / 2
+            u = 0
+            continue 
+        if u * u / u != u:
+            revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+        _103 = mem[64]
+        mem[64] = mem[64] + 64
+        mem[_103] = 26
+        mem[_103 + 32] = 'SafeMath: division by zero'
+        t = u * t / 10^18
+        idx = idx / 2
+        u = u * u / 10^18
+        continue 
+    _65 = mem[64]
+    mem[64] = mem[64] + 64
+    mem[_65] = 30
+    mem[_65 + 32] = 'SafeMath: subtraction overflow'
+    if 10^18 > t:
+        revert with 0, 'SafeMath: subtraction overflow'
+    _80 = mem[64]
+    mem[64] = mem[64] + 64
+    mem[_80] = 26
+    mem[_80 + 32] = 'SafeMath: division by zero'
+    if not arg3:
+        revert with 0, 'SafeMath: division by zero'
+    _93 = mem[64]
+    mem[64] = mem[64] + 64
+    mem[_93] = 26
+    mem[_93 + 32] = 'SafeMath: division by zero'
+    if (arg2 / arg3) + 10^18 < arg2 / arg3:
+        revert with 0, 'SafeMath: addition overflow'
+    u = 10^18
+    idx = arg3
+    v = (arg2 / arg3) + 10^18
+    while idx:
+        if not bool(idx):
+            if not v:
+                _157 = mem[64]
+                mem[64] = mem[64] + 64
+                mem[_157] = 26
+                mem[_157 + 32] = 'SafeMath: division by zero'
+                u = u
+                idx = idx / 2
+                v = 0
+                continue 
+            if v * v / v != v:
+                revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+            _163 = mem[64]
+            mem[64] = mem[64] + 64
+            mem[_163] = 26
+            mem[_163 + 32] = 'SafeMath: division by zero'
+            u = u
+            idx = idx / 2
+            v = v * v / 10^18
+            continue 
+        if not u:
+            _158 = mem[64]
+            mem[64] = mem[64] + 64
+            mem[_158] = 26
+            mem[_158 + 32] = 'SafeMath: division by zero'
+            if not v:
+                _190 = mem[64]
+                mem[64] = mem[64] + 64
+                mem[_190] = 26
+                mem[_190 + 32] = 'SafeMath: division by zero'
+                u = 0
+                idx = idx / 2
+                v = 0
+                continue 
+            if v * v / v != v:
+                revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+            _194 = mem[64]
+            mem[64] = mem[64] + 64
+            mem[_194] = 26
+            mem[_194 + 32] = 'SafeMath: division by zero'
+            u = 0
+            idx = idx / 2
+            v = v * v / 10^18
+            continue 
+        if v * u / u != v:
+            revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+        _164 = mem[64]
+        mem[64] = mem[64] + 64
+        mem[_164] = 26
+        mem[_164 + 32] = 'SafeMath: division by zero'
+        if not v:
+            _193 = mem[64]
+            mem[64] = mem[64] + 64
+            mem[_193] = 26
+            mem[_193 + 32] = 'SafeMath: division by zero'
+            u = v * u / 10^18
+            idx = idx / 2
+            v = 0
+            continue 
+        if v * v / v != v:
+            revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+        _195 = mem[64]
+        mem[64] = mem[64] + 64
+        mem[_195] = 26
+        mem[_195 + 32] = 'SafeMath: division by zero'
+        u = v * u / 10^18
+        idx = idx / 2
+        v = v * v / 10^18
+        continue 
+    _146 = mem[64]
+    mem[64] = mem[64] + 64
+    mem[_146] = 30
+    mem[_146 + 32] = 'SafeMath: subtraction overflow'
+    if 10^18 > u:
+        revert with 0, 'SafeMath: subtraction overflow'
+    if not s - 10^18 / arg3:
+        if not t - 10^18 / arg3:
+            revert with 0, 'SafeMath: division by zero'
+        return (0 / t - 10^18 / arg3)
+    if (-1 * 10^18 * s - 10^18 / arg3) + (u * s - 10^18 / arg3) / s - 10^18 / arg3 != u - 10^18:
+        revert with 0, 32, 33, 0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f, mem[mem[64] + 101 len 31]
+    if not t - 10^18 / arg3:
+        revert with 0, 'SafeMath: division by zero'
+    return ((-1 * 10^18 * s - 10^18 / arg3) + (u * s - 10^18 / arg3) / t - 10^18 / arg3)
+}
+
+
+
+}
