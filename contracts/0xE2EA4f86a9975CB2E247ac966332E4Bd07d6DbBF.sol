7c3169a53623622300e17ebc66072803b84691d6
bot <abagoforgans@users.noreply.github.com>
2022-04-08 03:13:12 +0000

    autocommit

---
+++ contracts/0xE2EA4f86a9975CB2E247ac966332E4Bd07d6DbBF.sol
@@ -0,0 +1,55 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+const tokenAddress = 0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7
+
+
+address owner;
+address stor1;
+
+function owner() payable {
+    return owner
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Ownable: new owner is the zero address'
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function sub_5faf30ad(?) payable {
+    require ext_code.size(stor1)
+    staticcall stor1.latestRoundData() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 160
+    require ext_call.return_data[0] == ext_call.return_data[22 len 10]
+    require ext_call.return_data[128] == ext_call.return_data[150 len 10]
+    return (10^10 * ext_call.return_data[32])
+}
+
+
+
+}
