1bcfcc46ea2d5f36e17bf0b1564d1788cd245249
bot <abagoforgans@users.noreply.github.com>
2022-04-15 08:15:01 +0000

    autocommit

---
+++ contracts/0xE00228D89c40e2168F7cD7d06FE60381D10E119D.sol
@@ -0,0 +1,575 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of uint8 sub_1a5b6774;
+uint256 sub_6f705899;
+uint256 sub_0de691e5;
+uint256 deadLine;
+uint8 stor4;
+uint8 storBCEF;
+uint8 stor108B;
+uint8 stor1993;
+uint8 stor1A3A;
+uint8 stor3111;
+uint8 stor4D78;
+uint8 stor4DDC;
+uint8 stor5095;
+uint8 stor84E8;
+uint8 storB2AA;
+uint8 storB991;
+uint8 storBBB0;
+uint8 storCF40;
+uint8 storE4CA;
+uint8 storE78F;
+uint8 storEA02;
+uint8 storF9CC;
+uint8 stor1058;
+uint8 stor11F5;
+uint8 stor1312;
+uint8 stor13F0;
+uint8 stor14BF;
+uint8 stor14D0;
+uint8 stor1500;
+uint8 stor154B;
+uint8 stor16C1;
+uint8 stor17F7;
+uint8 stor1A67;
+uint8 stor1ABD;
+uint8 stor1B07;
+uint8 stor1B8F;
+uint8 stor1DE1;
+uint8 stor1E9F;
+uint8 stor1EC0;
+uint8 stor1F96;
+uint8 stor21E3;
+uint8 stor22D8;
+uint8 stor230D;
+uint8 stor2327;
+uint8 stor2525;
+uint8 stor26DE;
+uint8 stor2831;
+uint8 stor2B04;
+uint8 stor2B6A;
+uint8 stor2BF7;
+uint8 stor2F2E;
+uint8 stor2F5B;
+uint8 stor30A3;
+uint8 stor315D;
+uint8 stor3190;
+uint8 stor324E;
+uint8 stor332B;
+uint8 stor3413;
+uint8 stor3435;
+uint8 stor357F;
+uint8 stor3594;
+uint8 stor35C2;
+uint8 stor35E2;
+uint8 stor36FF;
+uint8 stor3748;
+uint8 stor37A0;
+uint8 stor3891;
+uint8 stor38DD;
+uint8 stor393C;
+uint8 stor39BC;
+uint8 stor3A82;
+uint8 stor3AE8;
+uint8 stor3B16;
+uint8 stor3C5F;
+uint8 stor3C91;
+uint8 stor3DD8;
+uint8 stor3F15;
+uint8 stor3F2C;
+uint8 stor4294;
+uint8 stor4423;
+uint8 stor4590;
+uint8 stor46E2;
+uint8 stor4864;
+uint8 stor48E8;
+uint8 stor4B96;
+uint8 stor4BDC;
+uint8 stor4BF2;
+uint8 stor4C1E;
+uint8 stor4E98;
+uint8 stor4EC7;
+uint8 stor4FD7;
+uint8 stor4FD8;
+uint8 stor503A;
+uint8 stor521C;
+uint8 stor526A;
+uint8 stor541B;
+uint8 stor5453;
+uint8 stor549A;
+uint8 stor563C;
+uint8 stor5836;
+uint8 stor58AA;
+uint8 stor58BB;
+uint8 stor59E7;
+uint8 stor5A4B;
+uint8 stor5A88;
+uint8 stor5ADF;
+uint8 stor5C3B;
+uint8 stor5D9D;
+uint8 stor5E64;
+uint8 stor5FD1;
+uint8 stor6011;
+uint8 stor606E;
+uint8 stor6238;
+uint8 stor6378;
+uint8 stor63B5;
+uint8 stor6426;
+uint8 stor64D6;
+uint8 stor659B;
+uint8 stor65B1;
+uint8 stor6668;
+uint8 stor6E0A;
+uint8 stor6E59;
+uint8 stor6F5F;
+uint8 stor7011;
+uint8 stor713F;
+uint8 stor7207;
+uint8 stor7218;
+uint8 stor7221;
+uint8 stor73BC;
+uint8 stor73C9;
+uint8 stor73FE;
+uint8 stor751E;
+uint8 stor7531;
+uint8 stor756D;
+uint8 stor7591;
+uint8 stor7596;
+uint8 stor781B;
+uint8 stor7836;
+uint8 stor796D;
+uint8 stor79FF;
+uint8 stor7BB6;
+uint8 stor7EA0;
+uint8 stor7F20;
+uint8 stor7F2F;
+uint8 stor7F60;
+uint8 stor8026;
+uint8 stor80AF;
+uint8 stor81B0;
+uint8 stor81DE;
+uint8 stor82C1;
+uint8 stor82C8;
+uint8 stor84E0;
+uint8 stor85B2;
+uint8 stor85CA;
+uint8 stor867F;
+uint8 stor88E1;
+uint8 stor8BD8;
+uint8 stor8C2E;
+uint8 stor8DD5;
+uint8 stor9212;
+uint8 stor92FC;
+uint8 stor947C;
+uint8 stor95E7;
+uint8 stor9682;
+uint8 stor9915;
+uint8 stor9AEF;
+uint8 stor9C25;
+uint8 stor9C38;
+uint8 stor9C42;
+uint8 stor9C83;
+uint8 stor9DD6;
+uint8 stor9F97;
+uint8 storA0DE;
+uint8 storA1F8;
+uint8 storA265;
+uint8 storA287;
+uint8 storA54E;
+uint8 storA56F;
+uint8 storA6C6;
+uint8 storA868;
+uint8 storA9C5;
+uint8 storAB55;
+uint8 storAC55;
+uint8 storAEE3;
+uint8 storAF0D;
+uint8 storB18C;
+uint8 storB1BD;
+uint8 storB269;
+uint8 storB380;
+uint8 storB5A0;
+uint8 storB64B;
+uint8 storB765;
+uint8 storB772;
+uint8 storB920;
+uint8 storBB50;
+uint8 storBB71;
+uint8 storBBC3;
+uint8 storBC2C;
+uint8 storBDAE;
+uint8 storBFAE;
+uint8 storBFD5;
+uint8 storC08D;
+uint8 storC142;
+uint8 storC26A;
+uint8 storC58C;
+uint8 storC6BD;
+uint8 storC7C6;
+uint8 storC821;
+uint8 storC8B2;
+uint8 storC99F;
+uint8 storC9A4;
+uint8 storC9D1;
+uint8 storCAF7;
+uint8 storCB93;
+uint8 storCC03;
+uint8 storCE58;
+uint8 storCFEE;
+uint8 storD01F;
+uint8 storD0B5;
+uint8 storD0BA;
+uint8 storD12F;
+uint8 storD144;
+uint8 storD1B9;
+uint8 storD418;
+uint8 storD45F;
+uint8 storD4A5;
+uint8 storD502;
+uint8 storD770;
+uint8 storD8A3;
+uint8 storD968;
+uint8 storDAE7;
+uint8 storDB43;
+uint8 storDBFD;
+uint8 storDCA6;
+uint8 storDD40;
+uint8 storDEBA;
+uint8 storE1A5;
+uint8 storE278;
+uint8 storE31A;
+uint8 storE398;
+uint8 storE603;
+uint8 storE8C9;
+uint8 storEC50;
+uint8 storEC60;
+uint8 storEF7B;
+uint8 storF164;
+uint8 storF177;
+uint8 storF37D;
+uint8 storF5AC;
+uint8 storF610;
+uint8 storF66B;
+uint8 storF783;
+uint8 storF791;
+uint8 storFA13;
+uint8 storFAAE;
+uint8 storFB2C;
+uint8 storFC38;
+
+function sub_0de691e5(?) payable {
+    return sub_0de691e5
+}
+
+function sub_1a5b6774(?) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == address(arg1)
+    return sub_1a5b6774[arg1]
+}
+
+function sub_6f705899(?) payable {
+    return sub_6f705899
+}
+
+function deadLine() payable {
+    return deadLine
+}
+
+function _fallback() payable {
+    revert
+}
+
+function vote(uint8 arg1) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if block.timestamp > deadLine:
+        revert with 0, 'voting is over'
+    if arg1 != 2:
+        if arg1 != 3:
+            revert with 0, 'invalid vote'
+    if 2 == sub_1a5b6774[msg.sender]:
+        revert with 0, 'you have voted'
+    if 3 == sub_1a5b6774[msg.sender]:
+        revert with 0, 'you have voted'
+    if sub_1a5b6774[msg.sender] != 1:
+        revert with 0, 'only voter can vote'
+    sub_1a5b6774[msg.sender] = arg1
+    if arg1 != 2:
+        if sub_0de691e5 == -1:
+            revert with 0, 17
+        sub_0de691e5++
+    else:
+        if sub_6f705899 == -1:
+            revert with 0, 17
+        sub_6f705899++
+    emit 0x5579f999: msg.sender, arg1
+}
+
+function sub_3a6ae1b5(?) payable {
+    if stor4:
+        revert with 0, 'the function only call once'
+    stor4 = 1
+    storC821 = 1
+    stor1312 = 1
+    stor5C3B = 1
+    stor3DD8 = 1
+    stor5D9D = 1
+    stor867F = 1
+    storD4A5 = 1
+    stor1B07 = 1
+    storE78F = 1
+    stor7011 = 1
+    storAB55 = 1
+    stor4C1E = 1
+    storC26A = 1
+    stor14D0 = 1
+    stor92FC = 1
+    stor393C = 1
+    stor9682 = 1
+    storC8B2 = 1
+    stor2327 = 1
+    storB269 = 1
+    stor4864 = 1
+    storA265 = 1
+    storB18C = 1
+    stor3F2C = 1
+    storD144 = 1
+    stor3891 = 1
+    stor59E7 = 1
+    stor7BB6 = 1
+    storC7C6 = 1
+    stor357F = 1
+    stor4BF2 = 1
+    storAEE3 = 1
+    stor4BDC = 1
+    stor3C5F = 1
+    stor48E8 = 1
+    storB380 = 1
+    storE603 = 1
+    storD0B5 = 1
+    stor37A0 = 1
+    storDEBA = 1
+    storCB93 = 1
+    stor1EC0 = 1
+    stor3190 = 1
+    stor22D8 = 1
+    stor3594 = 1
+    storBB50 = 1
+    stor21E3 = 1
+    stor63B5 = 1
+    stor1993 = 1
+    stor1058 = 1
+    stor5E64 = 1
+    stor4E98 = 1
+    stor7F60 = 1
+    storEC50 = 1
+    stor4DDC = 1
+    stor64D6 = 1
+    storFB2C = 1
+    stor30A3 = 1
+    stor3F15 = 1
+    stor7221 = 1
+    storB2AA = 1
+    stor7836 = 1
+    stor606E = 1
+    stor503A = 1
+    stor3AE8 = 1
+    storBB71 = 1
+    storA868 = 1
+    stor9C42 = 1
+    stor85CA = 1
+    storA56F = 1
+    stor9C83 = 1
+    storCC03 = 1
+    stor154B = 1
+    storBDAE = 1
+    stor8C2E = 1
+    stor9F97 = 1
+    storB1BD = 1
+    stor9AEF = 1
+    stor3748 = 1
+    stor3C91 = 1
+    stor1DE1 = 1
+    stor73C9 = 1
+    stor3435 = 1
+    storAC55 = 1
+    storBC2C = 1
+    stor81B0 = 1
+    storFC38 = 1
+    stor1ABD = 1
+    stor5A88 = 1
+    stor526A = 1
+    stor521C = 1
+    storE8C9 = 1
+    storD418 = 1
+    stor36FF = 1
+    storF66B = 1
+    stor14BF = 1
+    storF164 = 1
+    stor5ADF = 1
+    stor58BB = 1
+    storF5AC = 1
+    stor549A = 1
+    storD770 = 1
+    storEC60 = 1
+    stor541B = 1
+    stor2F5B = 1
+    storE278 = 1
+    stor6378 = 1
+    stor8026 = 1
+    storD01F = 1
+    storB765 = 1
+    stor85B2 = 1
+    storC6BD = 1
+    storC08D = 1
+    storBFAE = 1
+    storD1B9 = 1
+    stor4FD7 = 1
+    stor4294 = 1
+    storE4CA = 1
+    stor35C2 = 1
+    storBFD5 = 1
+    storD45F = 1
+    stor4EC7 = 1
+    storDBFD = 1
+    storA54E = 1
+    stor332B = 1
+    stor81DE = 1
+    stor6F5F = 1
+    stor5453 = 1
+    stor82C8 = 1
+    stor6011 = 1
+    stor84E0 = 1
+    stor9DD6 = 1
+    stor5FD1 = 1
+    stor9C38 = 1
+    stor2B04 = 1
+    stor95E7 = 1
+    storCFEE = 1
+    stor35E2 = 1
+    stor39BC = 1
+    stor5A4B = 1
+    storA287 = 1
+    stor3A82 = 1
+    storF177 = 1
+    stor756D = 1
+    storE31A = 1
+    stor7F20 = 1
+    storEA02 = 1
+    storF610 = 1
+    stor7596 = 1
+    stor7F2F = 1
+    storB772 = 1
+    stor2BF7 = 1
+    stor6E0A = 1
+    storC9A4 = 1
+    stor315D = 1
+    storF791 = 1
+    storE1A5 = 1
+    stor16C1 = 1
+    storF783 = 1
+    storDCA6 = 1
+    stor563C = 1
+    storB64B = 1
+    storA1F8 = 1
+    stor2525 = 1
+    stor1A3A = 1
+    stor4D78 = 1
+    stor9C25 = 1
+    stor9212 = 1
+    stor1500 = 1
+    stor8BD8 = 1
+    storA9C5 = 1
+    stor13F0 = 1
+    storD968 = 1
+    stor1B8F = 1
+    stor5836 = 1
+    stor3413 = 1
+    stor6238 = 1
+    stor8DD5 = 1
+    storEF7B = 1
+    stor6426 = 1
+    stor7591 = 1
+    storCF40 = 1
+    stor73FE = 1
+    stor79FF = 1
+    stor713F = 1
+    storBCEF = 1
+    stor324E = 1
+    storC99F = 1
+    storD8A3 = 1
+    stor796D = 1
+    storFAAE = 1
+    storC9D1 = 1
+    stor26DE = 1
+    storBBC3 = 1
+    storFA13 = 1
+    stor659B = 1
+    storDD40 = 1
+    storB991 = 1
+    stor6668 = 1
+    stor947C = 1
+    stor11F5 = 1
+    stor73BC = 1
+    stor4590 = 1
+    storB920 = 1
+    storB5A0 = 1
+    stor7218 = 1
+    stor80AF = 1
+    stor4B96 = 1
+    stor7EA0 = 1
+    stor5095 = 1
+    storE398 = 1
+    storF37D = 1
+    stor3B16 = 1
+    storAF0D = 1
+    storC142 = 1
+    storF9CC = 1
+    storC58C = 1
+    stor7531 = 1
+    storD0BA = 1
+    storA6C6 = 1
+    stor9915 = 1
+    stor108B = 1
+    stor2F2E = 1
+    storCAF7 = 1
+    storDB43 = 1
+    storBBB0 = 1
+    stor1F96 = 1
+    stor3111 = 1
+    stor781B = 1
+    storCE58 = 1
+    stor4FD8 = 1
+    stor6E59 = 1
+    stor7207 = 1
+    stor2B6A = 1
+    stor2831 = 1
+    stor58AA = 1
+    stor84E8 = 1
+    stor65B1 = 1
+    stor38DD = 1
+    stor4423 = 1
+    stor751E = 1
+    stor17F7 = 1
+    storA0DE = 1
+    stor46E2 = 1
+    storD12F = 1
+    stor88E1 = 1
+    storDAE7 = 1
+    storD502 = 1
+    stor82C1 = 1
+    stor1A67 = 1
+    stor230D = 1
+    stor1E9F = 1
+}
+
+
+
+}
